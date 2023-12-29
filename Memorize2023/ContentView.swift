447fef43a2013ba0385c5a19c6f85fae7aa34148
Eduardo Thomas <eduardothomas64@gmail.com>
2023-12-29 00:39:33 -0500

    Initial Commit

---
+++ Memorize2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize2023
+//
+//  Created by Eduardo Thomas on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize2023/Memorize2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2023App.swift
+//  Memorize2023
+//
+//  Created by Eduardo Thomas on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
